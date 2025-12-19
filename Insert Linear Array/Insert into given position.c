e590b7f6130e7d609434efb78d15cdaf3e14eca2
Ayshi Paul Asha <ayshipaul00@gmail.com>
2025-12-19 20:34:43 +0600

    Update Insert into given position.c

--- Insert Linear Array/Insert into given position.c
+++ Insert Linear Array/Insert into given position.c
@@ -3,20 +3,20 @@ int main()
 {
     int arr[6]={1,2,3,4,5};
     int n=5;
-    int ITEM=5;
-    int p=3;
-    if(arr[p-1]=5)
+    int data=99;
+    int p=5;
+    for(int i=n; i>p-1; i--)
     {
-        arr[p-1]=ITEM;
+        arr[i] = arr[i-1];
     }
-    else
-    {
-
-    }
+     arr[p-1]=data;
+     n=n+1;
    for(int i=0; i<n; i++)
     {
         printf("%d\n", arr[i]);
     }
    return 0;
 }
+
+
 
